e5e24f74104d232e8597564e1794e1c4e0de5221
MJselorm <selormmatanawui2006@gmail.com>
2026-02-23 04:38:33 +0000

    Refactor LED control and remove unused code

--- JARVIS 4.1(esp)/jarvis_esp/jarvis_esp.ino
+++ JARVIS 4.1(esp)/jarvis_esp/jarvis_esp.ino
@@ -1,11 +1,10 @@
 #include <WiFi.h>
 
 // Replace with your Wi-Fi credentials
-const char* ssid = "MJ";
-const char* password = "PASSWORD";
+const char* ssid = "...";
+const char* password = "...";
 
 // LED pin
-const int ledPin = 2;
 const int rpin=32;
 const int gpin=33;
 const int bpin=25;
@@ -14,13 +13,11 @@ WiFiServer server(80);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
   pinMode(rpin, OUTPUT);
   pinMode(gpin, OUTPUT);
   pinMode(bpin, OUTPUT);
 
   //setting all leds off
-  digitalWrite(ledPin, LOW);
   digitalWrite(rpin, LOW);
   digitalWrite(gpin, LOW);
   digitalWrite(bpin, LOW);
@@ -50,9 +47,7 @@ void loop() {
         char c = client.read();
         request += c;
         if (c == '\n') { // End of HTTP request line
-          if (request.indexOf("GET /ON") >= 0) {
-            digitalWrite(ledPin, HIGH);
-          }
+
           if (request.indexOf("GET /RED") >= 0) {
             digitalWrite(rpin, HIGH);
             digitalWrite(gpin, LOW);
@@ -69,21 +64,10 @@ void loop() {
             digitalWrite(rpin, LOW);
             digitalWrite(gpin, LOW);
           }
-          if (request.indexOf("GET /WHITE") >= 0) {
-            analogWrite(rpin,255); 
-            analogWrite(gpin,255); 
-            analogWrite(bpin,255); 
-          }
-           if (request.indexOf("GET /YELLOW") >= 0) {
-            analogWrite(rpin,255); 
-            analogWrite(gpin,255); 
-            analogWrite(bpin,0); 
-          }
           if (request.indexOf("GET /OFF") >= 0) {
-            digitalWrite(ledPin, LOW);
-            digitalWrite(rPin, LOW);
-            digitalWrite(bPin, LOW);
-            digitalWrite(gpinin, LOW);
+            digitalWrite(rpin, LOW);
+            digitalWrite(bpin, LOW);
+            digitalWrite(gpin, LOW);
             
           }
 
@@ -94,7 +78,9 @@ void loop() {
           client.println("<!DOCTYPE html>");
           client.println("<html>");
           client.println("<h1>ESP32 LED Control</h1>");
-          client.println("<button onclick=\"window.location.href='/ON'\">LED ON</button>");
+          client.println("<button onclick=\"window.location.href='/RED'\">LED RED</button>");
+          client.println("<button onclick=\"window.location.href='/BLUE'\">LED ;BLUE</button>");
+          client.println("<button onclick=\"window.location.href='/GREEN'\">LED GREEN</button>");
           client.println("<button onclick=\"window.location.href='/OFF'\">LED OFF</button>");
           client.println("</html>");
           client.println();
